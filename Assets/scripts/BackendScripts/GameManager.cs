31996326bb7b4eeaac010ada026e4518f6bd7098
MohammedJamalDawoud <168332460+MohammedJamalDawoud@users.noreply.github.com>
2024-05-21 14:47:26 +0200

    Integrated Menu Manager in the Game to Open Different Menu

---
+++ Assets/scripts/BackendScripts/GameManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance; // this is the static instance of the GameManager Class
+    [Header("Menu Buttons")]
+    [SerializeField] private Button playMenuButton;
+    [SerializeField] private Button settingsMenuButton;
+    [SerializeField] private Button quitButton;
+    [Header("Back")]
+    [SerializeField] private Button backFromSettingButton;
+    private void Awake()
+    {
+        if (Instance == null) // if the instance of the GameManager is null then assign this to the instance
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject); // if the instance of the GameManager is not null then destroy this object
+        }
+        playMenuButton.onClick.RemoveAllListeners();
+        settingsMenuButton.onClick.RemoveAllListeners();
+        quitButton.onClick.RemoveAllListeners();
+        backFromSettingButton.onClick.RemoveAllListeners();
+
+        playMenuButton.onClick.AddListener(OnClickPlayMenuButton);
+        settingsMenuButton.onClick.AddListener(OnClickSettingsMenuButton);
+        quitButton.onClick.AddListener(OnClickQuitButton);
+        backFromSettingButton.onClick.AddListener(OnClickBackFromSettingButton);
+
+    }
+
+    public void OnClickPlayMenuButton()
+    {
+        MenuManager.Instance.OpenMenu("play");
+    }
+    public void OnClickSettingsMenuButton()
+    {
+        MenuManager.Instance.OpenMenu("setting");
+    }
+    public void OnClickQuitButton()
+    {
+        Application.Quit();
+    }
+    public void OnClickBackFromSettingButton()
+    {
+        MenuManager.Instance.OpenMenu("main");
+    }
+}
